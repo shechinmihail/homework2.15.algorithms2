d26290021e788b61038b1d43866ba685fe6f9db0
shechin <shechinmihail@mail.ru>
2022-12-12 21:07:55 +0500

    Начало ДЗ

---
+++ src/main/java/com/example/homework2_15_algorithms2/IntegerList.java
@@ -0,0 +1,4 @@
+package com.example.homework2_15_algorithms2;
+
+public interface IntegerList {
+}
---
+++ src/test/java/com/example/homework2_15_algorithms2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_15_algorithms2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
